c42dbb40759e3e6c90dcfa321f26cd17b7a62b9d
Edwin Waweru <ewaweru@tracom.co.ke>
2019-11-01 11:15:50 +0300

    added right db

--- tutorial/db/buildDB.bat
+++ tutorial/db/buildDB.bat
@@ -1,7 +1,7 @@
 @echo off
 SET DIR=%~d0%~p0%
 
-SET connstring="data source=EDWINLAPTOP\TUTORIAL;initial catalog=Northwind;integrated security=True"
+SET connstring="data source=EDWINLAPTOP;initial catalog=Northwind;integrated security=True"
 SET repository.path="https://github.com/edwinsentinel/tutorial-demo-net.git"
  
 "%DIR%..\..\packages\roundhouse.0.8.6\bin\rh.exe" /c=%connstring% /vf=../bin/tutorial.dll /r=%repository.path% /env=LOCAL /ni /simple
